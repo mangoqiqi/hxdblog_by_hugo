d6e767f166f7f988e76358ba43150fa267d39b30
黄显东 <xiandong.huang@hand-china.com>
2018-07-14 18:52:09 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ read line
 
 echo "$line"
 
-docker run -p 1313:1313 --name=hxdblogserver  hxdbolg:v1.0.0 hugo server --baseUrl=$line --bind=0.0.0.0
+docker run -d -p 1313:1313 --name=hxdblogserver  hxdbolg:v1.0.0 hugo server --baseUrl=$line --bind=0.0.0.0
 
 
